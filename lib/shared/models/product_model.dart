e879962560f1e99ef3b68f8277460a2b3754f3bc
dan7sc <su7cheng@gmail.com>
2021-07-25 14:33:35 -0300

    Add id to ProductModel

--- lib/shared/models/product_model.dart
+++ lib/shared/models/product_model.dart
@@ -1,7 +1,10 @@
 class ProductModel {
+  final String id;
   final String name;
   final double price;
+
   ProductModel({
+    required this.id,
     required this.name,
     required this.price,
   });
